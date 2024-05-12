6f9321925d8f1a832b867b4e4dd6b0b894589c64
BorysShulyak <borysshulyak@gmail.com>
2024-05-12 13:12:42 +0300

    feat(components/button): add all the necessary buttons

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,231 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ButtonsApartAnonymousActionButton extends Schema.Component {
+  collectionName: 'cmp_apart_anon_action_buttons';
+  info: {
+    displayName: 'Apartment Anonymous Action Button';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiButtonAction: Attribute.Enumeration<['openLink', 'openInfoPage']> &
+      Attribute.Required &
+      Attribute.DefaultTo<'openLink'>;
+    muiButtonLink: Attribute.String;
+    settings: Attribute.Component<'global-components.settings-json'>;
+    componentGridItemSettings: Attribute.Component<'global-components.settings-json'>;
+    useBreakpoint: Attribute.Component<'global-components.use-breakpoint'> &
+      Attribute.Required;
+  };
+}
+
+export interface ButtonsApartAuthenticatedActionButton
+  extends Schema.Component {
+  collectionName: 'cmp_apart_auth_action_buttons';
+  info: {
+    displayName: 'Apartment Authenticated Action Button';
+    description: '';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiButtonAction: Attribute.Enumeration<
+      ['openInfoPage', 'book', 'toggleFavorite', 'openLink']
+    > &
+      Attribute.Required &
+      Attribute.DefaultTo<'openLink'>;
+    muiButtonLink: Attribute.String;
+    settings: Attribute.Component<'global-components.settings-json'>;
+    componentGridItemSettings: Attribute.Component<'global-components.settings-json'>;
+    useBreakpoint: Attribute.Component<'global-components.use-breakpoint'> &
+      Attribute.Required;
+    muiIconButtonIconActive: Attribute.Media;
+    muiButtonStartIconActive: Attribute.Media;
+    muiButtonTextActive: Attribute.String;
+    muiButtonEndIconActive: Attribute.Media;
+  };
+}
+
+export interface ButtonsBannerAnonymousActionButton extends Schema.Component {
+  collectionName: 'cmp_banner_anon_action_buttons';
+  info: {
+    displayName: 'Banner Anonymous Action Button';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiButtonAction: Attribute.Enumeration<['openLink']> &
+      Attribute.Required &
+      Attribute.DefaultTo<'openLink'>;
+    muiButtonLink: Attribute.String;
+    settings: Attribute.Component<'global-components.settings-json'>;
+    componentGridItemSettings: Attribute.Component<'global-components.settings-json'>;
+    useBreakpoint: Attribute.Component<'global-components.use-breakpoint'> &
+      Attribute.Required;
+  };
+}
+
+export interface ButtonsBannerAuthenticatedActionButton
+  extends Schema.Component {
+  collectionName: 'cmp_banner_auth_action_buttons';
+  info: {
+    displayName: 'Banner Authenticated Action Button';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiButtonAction: Attribute.Enumeration<['openLink']> &
+      Attribute.Required &
+      Attribute.DefaultTo<'openLink'>;
+    muiButtonLink: Attribute.String;
+    settings: Attribute.Component<'global-components.settings-json'>;
+    componentGridItemSettings: Attribute.Component<'global-components.settings-json'>;
+    useBreakpoint: Attribute.Component<'global-components.use-breakpoint'> &
+      Attribute.Required;
+  };
+}
+
+export interface ButtonsGlobalAnonymousActionButton extends Schema.Component {
+  collectionName: 'cmp_global_anon_action_buttons';
+  info: {
+    displayName: 'Global Anonymous Action Button';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiButtonAction: Attribute.Enumeration<
+      ['openSidebar', 'closeSidebar', 'openLink', 'replaceLink']
+    > &
+      Attribute.Required &
+      Attribute.DefaultTo<'openLink'>;
+    muiButtonLink: Attribute.String;
+    settings: Attribute.Component<'global-components.settings-json'>;
+    componentGridItemSettings: Attribute.Component<'global-components.settings-json'>;
+    useBreakpoint: Attribute.Component<'global-components.use-breakpoint'> &
+      Attribute.Required;
+  };
+}
+
+export interface ButtonsGlobalAuthenticatedActionButton
+  extends Schema.Component {
+  collectionName: 'cmp_global_auth_action_buttons';
+  info: {
+    displayName: 'Global Authenticated Action Button';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiButtonAction: Attribute.Enumeration<
+      ['openSidebar', 'closeSidebar', 'logout', 'openLink']
+    > &
+      Attribute.Required &
+      Attribute.DefaultTo<'openLink'>;
+    muiButtonLink: Attribute.String;
+    settings: Attribute.Component<'global-components.settings-json'>;
+    componentGridItemSettings: Attribute.Component<'global-components.settings-json'>;
+    useBreakpoint: Attribute.Component<'global-components.use-breakpoint'> &
+      Attribute.Required;
+  };
+}
+
+export interface ButtonsGlobalGenericActionButton extends Schema.Component {
+  collectionName: 'cmp_global_generic_action_buttons';
+  info: {
+    displayName: 'Global Generic Action Button';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiButtonAction: Attribute.Enumeration<
+      ['openSidebar', 'closeSidebar', 'openLink']
+    > &
+      Attribute.Required &
+      Attribute.DefaultTo<'openLink'>;
+    muiButtonLink: Attribute.String;
+    settings: Attribute.Component<'global-components.settings-json'>;
+    componentGridItemSettings: Attribute.Component<'global-components.settings-json'>;
+    useBreakpoint: Attribute.Component<'global-components.use-breakpoint'> &
+      Attribute.Required;
+  };
+}
+
+export interface ButtonsSpecialtyButton extends Schema.Component {
+  collectionName: 'cmp_specialty_buttons';
+  info: {
+    displayName: 'Specialty Button';
+    icon: 'adjust';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    settings: Attribute.Component<'global-components.settings-json'>;
+  };
+}
+
+export interface ButtonsSpecialtyLinkButton extends Schema.Component {
+  collectionName: 'cmp_specialty_link_buttons';
+  info: {
+    displayName: 'Specialty Link Button';
+    icon: 'allergies';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiButtonLink: Attribute.String & Attribute.Required;
+    settings: Attribute.Component<'global-components.settings-json'>;
+  };
+}
+
+export interface ButtonsSpecialtyStatefulButton extends Schema.Component {
+  collectionName: 'cmp_specialty_stateful_buttons';
+  info: {
+    displayName: 'Specialty Stateful Button';
+    description: '';
+  };
+  attributes: {
+    isMuiIconButton: Attribute.Boolean & Attribute.DefaultTo<false>;
+    muiIconButtonIcon: Attribute.Media;
+    muiButtonText: Attribute.String;
+    muiButtonStartIcon: Attribute.Media;
+    muiButtonEndIcon: Attribute.Media;
+    muiIconButtonIconActive: Attribute.Media;
+    muiButtonTextActive: Attribute.String;
+    muiButtonStartIconActive: Attribute.Media;
+    muiButtonEndIconActive: Attribute.Media;
+    settings: Attribute.Component<'global-components.settings-json'>;
+    muiButtonUseActiveState: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<false>;
+  };
+}
+
 export interface GlobalComponentsSettingsJson extends Schema.Component {
   collectionName: 'cmp_settings_jsons';
   info: {
@@ -68,6 +294,16 @@ export interface GlobalComponentsUseBreakpoint extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'buttons.apart-anonymous-action-button': ButtonsApartAnonymousActionButton;
+      'buttons.apart-authenticated-action-button': ButtonsApartAuthenticatedActionButton;
+      'buttons.banner-anonymous-action-button': ButtonsBannerAnonymousActionButton;
+      'buttons.banner-authenticated-action-button': ButtonsBannerAuthenticatedActionButton;
+      'buttons.global-anonymous-action-button': ButtonsGlobalAnonymousActionButton;
+      'buttons.global-authenticated-action-button': ButtonsGlobalAuthenticatedActionButton;
+      'buttons.global-generic-action-button': ButtonsGlobalGenericActionButton;
+      'buttons.specialty-button': ButtonsSpecialtyButton;
+      'buttons.specialty-link-button': ButtonsSpecialtyLinkButton;
+      'buttons.specialty-stateful-button': ButtonsSpecialtyStatefulButton;
       'global-components.settings-json': GlobalComponentsSettingsJson;
       'global-components.slider-settings-json': GlobalComponentsSliderSettingsJson;
       'global-components.theme': GlobalComponentsTheme;
