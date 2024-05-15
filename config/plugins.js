dacda6debb9bfe1c93405a113516178fb0fb1976
BorysShulyak <borysshulyak@gmail.com>
2024-05-15 07:57:03 +0300

    feat(plugins): add `graphql` plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = () => ({
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 27,
+      defaultLimit: -1,
+      apolloServer: {
+        introspection: true
+      }
+    },
+  },
+});
